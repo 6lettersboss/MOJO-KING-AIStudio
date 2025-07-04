854d45adf5a71b60043f8d10e3f7b70822e00313
6lettersboss <sungjin.kim@6letters.co.kr>
2025-07-04 12:53:30 +0900

    Delete mojo-king (2) directory

--- mojo-king (2)/Dockerfile/Dockerfile.tsx
+++
@@ -1,3 +0,0 @@
-// This file is incorrect and has been deprecated.
-// Please use the 'Dockerfile' located in the project root.
-// This file and its parent directory can now be safely deleted.
--- mojo-king (2)/server.js
+++
@@ -1,3 +0,0 @@
-// This file is obsolete and can be deleted.
-// The Express server has been replaced by a Vercel Serverless Function 
-// located at `api/generate-report.js` for deployment.
